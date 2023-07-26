a96dbe895b928259df6cb49d0377c096fa5a43a3
Ahmed Hany <dev.ahmedhany@gmail.com>
2023-07-26 10:27:44 +0300

    es5

--- main.js
+++ main.js
@@ -1,27 +1,29 @@
-const log = (error) => {
-  const div = document.createElement("div");
-  div.style.color = "red";
-  div.innerHTML = error;
-  document.body.appendChild(div);
+var log = function(error) {
+  // const div = document.createElement("div");
+  // div.style.color = "red";
+  // div.innerHTML = error;
+  // document.body.appendChild(div);
+  co1nsole.error(error);
 };
-function createLinearGradientHelper(gradientColors) {
+
+var createLinearGradientHelper = function(gradientColors) {
   try {
-    const WIDTH = 101; // 0 to 100
-    const HEIGHT = 1;
+    var WIDTH = 101; // 0 to 100
+    var HEIGHT = 1;
 
     // Canvas
-    const canvasElement = document.createElement("CANVAS");
+    var canvasElement = document.createElement('CANVAS');
     canvasElement.width = WIDTH;
     canvasElement.height = HEIGHT;
 
-    const context = canvasElement.getContext("2d", {
+    var context = canvasElement.getContext('2d', {
       willReadFrequently: true,
     });
 
     // Gradient
-    const gradient = context.createLinearGradient(0, 0, WIDTH, 0); // x0, y0, x1, y1
+    var gradient = context.createLinearGradient(0, 0, WIDTH, 0); // x0, y0, x1, y1
 
-    gradientColors.forEach((val) => {
+    gradientColors.forEach(function(val) {
       gradient.addColorStop(val[1], val[0]);
     });
 
@@ -29,100 +31,113 @@ function createLinearGradientHelper(gradientColors) {
     context.fillStyle = gradient;
     context.fillRect(0, 0, WIDTH, HEIGHT); // x, y, width, height
 
-    function getColor(percent) {
-      const color = context.getImageData(parseInt(percent), 0, 1, 1); // x, y, width, height
-      const rgba = color.data;
+    var getColor = function(percent) {
+      var color = context.getImageData(parseInt(percent), 0, 1, 1); // x, y, width, height
+      var rgba = color.data;
 
-      return `rgb(${rgba[0]}, ${rgba[1]}, ${rgba[2]})`;
-    }
+      return 'rgb(' + rgba[0] + ',' + rgba[1] + ',' + rgba[2] + ')';
+    };
 
     return {
-      getColor,
+      getColor: getColor,
     };
   } catch (error) {
     log(error);
   }
-}
+};
 
-const grad = createLinearGradientHelper([
-  ["#000", 0],
-  ["#FFF", 1],
+var grad = createLinearGradientHelper([
+  ['#000', 0],
+  ['#FFF', 1],
 ]);
 
-let size = 60;
+var size = 60;
 
-function populate(size) {
+var populate = function(size) {
   try {
-    const container = document.querySelector(".container");
-    for (let i = 0; i < size * size; i++) {
-      const div = document.createElement("div");
-      div.classList.add("pixel");
-
+    var container = document.querySelector('.container');
+    for (var i = 0; i < size * size; i++) {
+      var div = document.createElement('div');
+      div.classList.add('pixel');
       container.appendChild(div);
     }
   } catch (error) {
     log(error);
   }
-}
+};
 
 populate(size);
 
-async function fetchWithTimeout(resource, options = {}) {
+var fetchWithTimeout = function(resource, options) {
+  if (options === void 0) {
+    options = {};
+  }
+
+  return new Promise(function(resolve, reject) {
+    var timeout = options.timeout || 1000;
+    var controller = new AbortController();
+    var id = setTimeout(function() {
+      controller.abort();
+    }, timeout);
+
+    fetch(resource, Object.assign({}, options, { signal: controller.signal }))
+      .then(function(response) {
+        clearTimeout(id);
+        resolve(response);
+      })
+      .catch(function(error) {
+        clearTimeout(id);
+        reject(error);
+      });
+  }).catch(function(error) {
+    log(error);
+  });
+};
+
+var checkResponseTime = function(testURL, options) {
   try {
-    const { timeout = 1000 } = options;
-
-    const controller = new AbortController();
-    const id = setTimeout(() => controller.abort(), timeout);
-
-    const response = await fetch(resource, {
-      ...options,
-      signal: controller.signal,
-    });
-    clearTimeout(id);
-    return response;
+    var time1 = performance.now();
+    return fetchWithTimeout(testURL, options)
+      .then(function(res) {
+        if (!res.ok) {
+          return 0;
+        }
+        var time = performance.now() - time1;
+        var timeout = (options && options.timeout) || 1000;
+        return time > timeout ? timeout : time;
+      })
+      .catch(function(error) {
+        log(error);
+        return 0;
+      });
   } catch (error) {
     log(error);
   }
-}
+};
 
-async function checkResponseTime(testURL, options) {
+var i = 0;
+
+var printPing = function(host) {
   try {
-    let time1 = performance.now();
-    const res = await fetchWithTimeout(testURL, options);
-    if (!res.ok) {
-      return 0;
-    }
-    let time = performance.now() - time1;
-    const timeout = options?.timeout || 1000;
-    return time > timeout ? timeout : time;
-  } catch (error) {
-    log(error);
-    return 0;
-  }
-}
-
-let i = 0;
-
-const printPing = (host) => {
-  try {
-    const timeout = 1000;
-    setInterval(async () => {
-      const url = host + "/favicon.ico?no-cache=" + +new Date();
-      const data = await checkResponseTime(url, { timeout });
-      const elements = document.querySelectorAll(".pixel");
-      if (data === 0) {
-        elements[i].style.background = "#a00";
-      } else {
-        elements[i].style.background = grad.getColor((data / timeout) * 100);
-      }
-      i++;
-      if (i == size * size) i = 0;
-      elements[i].style.background = "rgb(61, 61, 61)";
+    var timeout = 1000;
+    setInterval(function() {
+      var url = host + '/favicon.ico?no-cache=' + +new Date();
+      checkResponseTime(url, { timeout: timeout }).then(function(data) {
+        var elements = document.querySelectorAll('.pixel');
+        if (data === 0) {
+          elements[i].style.background = '#a00';
+        } else {
+          elements[i].style.background = grad.getColor((data / timeout) * 100);
+        }
+        i++;
+        if (i == size * size) i = 0;
+        elements[i].style.background = 'rgb(61, 61, 61)';
+      });
     }, timeout);
   } catch (error) {
     log(error);
   }
 };
 
-printPing(".");
-// printPing("https://www.google.com");
+//printPing('.');
+printPing('https://www.google.com');
